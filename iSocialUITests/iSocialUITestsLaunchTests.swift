ea6c2b21181c7ddd863b009eeaccf471d34c10c6
Thao_Vo <thaovo2962002@gmail.com>
2022-12-16 10:32:48 +0700

    Initial commit

---
+++ iSocialUITests/iSocialUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  iSocialUITestsLaunchTests.swift
+//  iSocialUITests
+//
+//  Created by MacOS on 16/12/2022.
+//
+
+import XCTest
+
+final class iSocialUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
